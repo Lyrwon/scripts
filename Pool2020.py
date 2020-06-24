b2e5f549ac986e0098c619dc767cf4ad2f012e22
Horn <horn@indibit.eu>
2020-06-24 21:25:42 +0200

    Update Pool2020.py

--- Pool2020.py
+++ Pool2020.py
@@ -12,14 +12,29 @@ K1   /      X   Außenleiter
     |
     |__     X   Leer
 """
-import sys
-import subprocess
+
 import os
 import os.path
 import RPi.GPIO as GPIO
 from datetime import datetime, time, timezone
+import time
 from datetime import datetime as DT
 from datetime import date as Date
+
+def write_to_log(msg):
+    file_name = "/opt/pool.log"
+    output = open(file_name, 'a')
+    jetzt = datetime.now().strftime("%H:%M:%S")
+    output.write(msg + " " + jetzt + "\n")
+    print(msg + " " + jetzt)
+    output.close()
+
+def GPIO_14_on():
+    GPIO.output(14, GPIO.LOW)
+
+def GPIO_14_off():
+    GPIO.output(14, GPIO.HIGH)
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -29,59 +44,49 @@ GPIO.output(14, GPIO.HIGH)
 feierabend = False
 pumpe_an = False
 
-def write_to_log(msg):
-    file_name = "/opt/pool.log"
-    output = open(file_name, 'a')
-    jetzt = datetime.now().strftime("%H_%M_%S")
-    output.write(jetzt + " " + msg)
-    output.close()
-
+write_to_log("Programm gestartet um: ")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 while True:
-    #zwischen 10 und 17 Uhr
-    if datetime.now().strftime("%H") > 10 and datetime.now().strftime("%H") <= 16:
-        #write_to_log("if datetime.now().strftime(\"%H\") > 10 and datetime.now().strftime(\"%H\") <= 16:")
-        #print("if datetime.now().strftime("%H") > 10 or datetime.now().strftime("%H") <= 16:")
+    #zwischen 9 und 17 Uhr
+    if int(datetime.now().strftime("%H")) > 8 and int(datetime.now().strftime("%H")) <= 17:
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         #Beim Umstellen auf Arbeit wird feierabend auf False gesetzt
         if feierabend == True:
-            write_to_log("Pumpe arbeitet seit ",datetime.now().strftime("%H") , datetime.now().strftime("%M") , datetime.now().strftime("%S"))
+            write_to_log("Pumpe arbeitet seit: ")
             feierabend = False
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         #bei 0,10,20,30,40,50 Min und weniger als 21 sec wird die Pumpe angeschaltet
-        if datetime.now().strftime("%M") % 10 == 0 and datetime.now().strftime("%S") < 21 and pumpe_an == False:
-            print ("Pumpe an um ", datetime.now().strftime("%H") , datetime.now().strftime("%M") , datetime.now().strftime("%S"))
+        if int(datetime.now().strftime("%M")) % 10 == 0 and int(datetime.now().strftime("%S")) < 21  and pumpe_an == False:
+            write_to_log("Pumpe an um: ")
             pumpe_an = True
-            GPIO.output(14,GPIO.LOW)
+            GPIO_14_on()
             time.sleep(20)
 
         #andernfalls ist die Pumpe aus
         else :
             if pumpe_an == True:
-                print ("Pumpe aus um ", datetime.now().strftime("%H") , datetime.now().strftime("%M") , datetime.now().strftime("%S"))
+                write_to_log("Pumpe aus um: ")
                 pumpe_an = False
-                GPIO.output(14,GPIO.HIGH)
-
+                GPIO_14_off()
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     else:
         if feierabend == False:
             feierabend = True
-            print ("Pumpe Feierabend um ", datetime.now().strftime("%H") , datetime.now().strftime("%M") , datetime.now().strftime("%S"))
-        GPIO.output(14,GPIO.HIGH)
+        GPIO_14_off()
         pause = True
+        write_to_log("Feierabend: ")
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         #solange in der Schleife bis aktuelle Uhrzeit mindestens 10:00 Uhr
         while pause:
             now = time.localtime()
-            if datetime.now().strftime("%H") > 9 and datetime.now().strftime("%H") < 17:
+            if int(datetime.now().strftime("%H")) > 9 and int(datetime.now().strftime("%H")) < 17:
                 pause = False
             time.sleep(1)
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    now = time.localtime()
-    #print(datetime.now().strftime("%S"))
     time.sleep(1)
+
